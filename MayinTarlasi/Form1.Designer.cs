fdffdcb11fe639765b837f96dfc9465e72d562c7
Burak Özkaya <ozkayaburak97@gmail.com>
2023-08-10 08:50:38 +0300

    Add project files.

---
+++ MayinTarlasi/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace MayinTarlasi
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            timer1 = new System.Windows.Forms.Timer(components);
+            label1 = new Label();
+            label2 = new Label();
+            btnBaslat = new Button();
+            txtMayin = new TextBox();
+            txtSure = new TextBox();
+            timer2 = new System.Windows.Forms.Timer(components);
+            SuspendLayout();
+            // 
+            // timer1
+            // 
+            timer1.Interval = 1000;
+            timer1.Tick += timer1_Tick;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(501, 44);
+            label1.Name = "label1";
+            label1.Size = new Size(19, 15);
+            label1.TabIndex = 0;
+            label1.Text = "00";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(430, 44);
+            label2.Name = "label2";
+            label2.Size = new Size(65, 15);
+            label2.TabIndex = 1;
+            label2.Text = "Kalan Süre:";
+            // 
+            // btnBaslat
+            // 
+            btnBaslat.Location = new Point(430, 120);
+            btnBaslat.Name = "btnBaslat";
+            btnBaslat.Size = new Size(109, 40);
+            btnBaslat.TabIndex = 2;
+            btnBaslat.Text = "Başlat";
+            btnBaslat.UseVisualStyleBackColor = true;
+            btnBaslat.Click += btnBaslat_Click;
+            // 
+            // txtMayin
+            // 
+            txtMayin.Location = new Point(430, 62);
+            txtMayin.Name = "txtMayin";
+            txtMayin.PlaceholderText = "Mayın sayısını girin";
+            txtMayin.Size = new Size(109, 23);
+            txtMayin.TabIndex = 3;
+            // 
+            // txtSure
+            // 
+            txtSure.Location = new Point(430, 91);
+            txtSure.Name = "txtSure";
+            txtSure.PlaceholderText = "Süre bilgisini girin";
+            txtSure.Size = new Size(109, 23);
+            txtSure.TabIndex = 4;
+            // 
+            // timer2
+            // 
+            timer2.Interval = 1000;
+            timer2.Tick += timer2_Tick;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(579, 395);
+            Controls.Add(txtSure);
+            Controls.Add(txtMayin);
+            Controls.Add(btnBaslat);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private Label label1;
+        private Label label2;
+        private Button btnBaslat;
+        private TextBox txtMayin;
+        private TextBox txtSure;
+        private System.Windows.Forms.Timer timer2;
+    }
+}
---
+++ MayinTarlasi/Form1.cs
@@ -0,0 +1,188 @@
+﻿namespace MayinTarlasi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+
+            InitializeComponent();
+        }
+        Dictionary<(int, int), string> dic = new Dictionary<(int, int), string>();
+        Dictionary<(int, int), string> dicN = new Dictionary<(int, int), string>();
+        DialogResult tekrarOyna;
+
+        (int, int) temp;
+        int count;
+        int count2 = 10;
+
+        Button[,] buttons = new Button[10, 10];
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Name = "Mayın Tarlası";
+            LabelSetter();
+            this.AutoSize = false;
+            this.Size = new Size(600, 400);
+        }
+
+
+
+
+
+        private void BombCheck(object? sender, MouseEventArgs e)
+        {
+            Button btn = sender as Button;
+            int x, y;
+            x = 1;
+            y = 1;
+            for (int i = 0; i < 10; i++)
+                for (int j = 0; j < 10; j++)
+                {
+                    if (buttons[i, j] == btn)
+                    {
+                        x = i;
+                        y = j;
+                        i = 10;
+                        j = 10;
+                    }
+                }
+            temp = (x, y);
+            if (dic.ContainsKey(temp))
+            {
+                timer2.Enabled = true;
+                timer2.Start();
+            }
+            dicN.Remove((x, y));
+            buttons[x, y].Enabled = false;
+            buttons[x, y].BackColor = Color.Green;
+            if (dicN.Count == 0)
+            {
+                MessageBox.Show("Tebrikler Oynu kazandınız");
+                DialogRes();
+            }
+
+            int count = 0;
+            for (int i = -1; i <= 1; i++)
+            {
+                for (int j = -1; j <= 1; j++)
+                {
+                    if (i == 0 && j == 0)
+                        continue;
+                    if (dic.ContainsKey((x + i, y + j)))
+                    {
+                        count++;
+                    }
+
+                }
+            }
+            btn.Text = count.ToString();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            count--;
+            LabelSetter();
+            if (count == 0)
+            {
+                foreach (var item in buttons)
+                {
+                    item.Enabled = false;
+                }
+                timer1.Stop();
+                MessageBox.Show("Süreniz dolmuştur");
+                DialogRes();
+            }
+        }
+
+        private void LabelSetter()
+        {
+            string tempSaniye = count % 60 < 10 ? "0" + (count % 60).ToString() : (count % 60).ToString();
+            int tempDakika = (count / 60);
+            label1.Text = tempDakika > 0 ? $"{tempDakika}:{tempSaniye}" : $"{tempSaniye}";
+        }
+
+        private void BombMaker(int bomb, int x)
+        {
+            for (int i = 0; i < bomb; i++)
+            {
+                temp = (new Random().Next(0, x), new Random().Next(0, x));
+                if (!dic.ContainsKey(temp))
+                    dic.Add(temp, "bomb");
+                else
+                    i--;
+            }
+        }
+        private void TableMaker(int temp)
+        {
+
+            int x = 0;
+            int y = 0;
+            for (int i = 0; i < temp; i++)
+            {
+                for (int j = 0; j < temp; j++)
+                {
+
+                    buttons[i, j] = new Button()
+                    {
+
+                        Size = new Size(30, 30),
+                        Location = new Point(x, y),
+
+                    };
+
+                    //if (dic.ContainsKey((i, j)))
+                    //    buttons[i, j].Text = "bomb";
+                    if (!dic.ContainsKey((i, j)))
+                        dicN.Add((i, j), "neutral");
+                    x += 31;
+                    buttons[i, j].TabStop = false;
+
+
+                    this.Controls.Add(buttons[i, j]);
+                    buttons[i, j].MouseClick += BombCheck;
+                }
+
+                y += 30;
+                x = 0;
+            }
+        }
+
+        private void DialogRes()
+        {
+            tekrarOyna = MessageBox.Show("Devam etmek istiyor musunuz ?", "Mayın Tarlası", MessageBoxButtons.YesNo);
+            if (tekrarOyna == DialogResult.Yes)
+            {
+                Application.Restart();
+                Environment.Exit(0);
+            }
+            else
+            {
+                MessageBox.Show("Uygulama sonlandırıldı");
+                Application.Exit();
+            }
+        }
+
+        private void btnBaslat_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+            timer1.Start();
+            count = int.Parse(txtSure.Text);
+            int tempBomb = int.Parse(txtMayin.Text);
+            int temp = 10;
+            BombMaker(tempBomb, temp);
+            TableMaker(temp);
+
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            count2--;
+            if(count2 == 0)
+            {
+
+                timer2.Stop();
+                MessageBox.Show("Oynu kaybettiniz.");
+                DialogRes();
+            }
+        }
+    }
+}
